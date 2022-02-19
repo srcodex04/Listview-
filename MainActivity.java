155ab4800839eb363bc46f18b5842a27c27c3192
SR CodeX <99443296+srcodex04@users.noreply.github.com>
2022-02-19 12:24:46 +0530

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,62 @@
+package com.srcodex.msa;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.listview);
+
+        final ArrayList<String> list = new ArrayList<>();
+        list.add("one");
+        list.add("two");
+        list.add("three");
+        list.add("four");
+        list.add("five");
+        list.add("six");
+        list.add("seven");
+        list.add("eight");
+        list.add("nine");
+        list.add("ten");
+        list.add("eleven");
+        list.add("twelve");
+        list.add("thirteen");
+        list.add("fourteen");
+        list.add("fifteen");
+        list.add("sixteen");
+        list.add("seventeen");
+        list.add("eighteen");
+        list.add("nineteen");
+        list.add("twenty");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, list);
+
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                Toast.makeText(MainActivity.this, "Clicked "+list.get(position), Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+
+    }
+}
